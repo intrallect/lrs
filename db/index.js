4b0c60ed4460f7e40040f67a2a8192142c7449ec
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2017-02-03 11:57:26 +0000

    added support for DBURI env var

--- db/index.js
+++ db/index.js
@@ -3,11 +3,19 @@
 var monk = require('monk');
 var constants = require('../constants');
 
-var
-    dbhost = (process.env.DBHOST || process.env.IP || '127.0.0.1'),
-    dbname = (process.env.DBNAME || 'lrs'),
-    url = 'mongodb://' + dbhost + '/' + dbname,
-    db = monk(url, { connectTimeoutMS: constants.dbConnectionTimeout, socketTimeoutMS: constants.dbSocketTimeout }),
+function getURI() {
+  if (process.env.DBURI) {
+    return process.env.DBURI;
+  }
+
+  var dbhost = (process.env.DBHOST || process.env.IP || '127.0.0.1'),
+      dbname = (process.env.DBNAME || 'lrs');
+
+  return 'mongodb://' + dbhost + '/' + dbname;
+}
+
+var dbOpts = { connectTimeoutMS: constants.dbConnectionTimeout, socketTimeoutMS: constants.dbSocketTimeout },
+    db = monk(getURI(), dbOpts),
     statements = db.get('statements'),
     results = db.get('results');
 
